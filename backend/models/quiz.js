8a9178cf553f5d6399e710f306194f98ff7a89fa
kamalvk18 <kamalbathula007@gmail.com>
2023-07-28 19:58:09 +0530

    models modification and new routes added

---
+++ backend/models/quiz.js
@@ -0,0 +1,59 @@
+var mongoose = require("mongoose")
+
+const optionSchema = new mongoose.Schema({
+    option: {
+      type: String,
+      required: true
+    },
+  
+    isAnswer: {
+      type: Boolean,
+      default: false
+    }
+  })
+
+const questionSchema = new mongoose.Schema({
+  question: {
+    type: String,
+    required: true
+  },
+  timeInSec:{
+    type:Number,
+    default:30
+  },
+  options: [
+    optionSchema
+  ],
+})
+
+const quizschema=new mongoose.Schema({
+  quizName:{
+    type:String,
+    require: true
+  },
+
+  subjectName: {
+    type: String,
+    required: true
+  },
+
+  description: String,
+
+  setBy:{
+    type: String,
+    required: true
+  },
+
+  collegeName:{
+    type:String,
+    required:true
+  },
+
+  questions: [
+    questionSchema
+  ],
+
+  totalMarks: Number
+})
+
+module.exports = mongoose.model("quiz",quizschema)
