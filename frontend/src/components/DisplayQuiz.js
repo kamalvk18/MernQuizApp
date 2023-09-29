9b1ecb564bcb8d571dc373bb81b5a87868c69e1f
haneesh9840 <dabbiruhaneesh@gmail.com>
2023-09-29 23:38:56 +0530

    Max attempts functionality

--- frontend/src/components/DisplayQuiz.js
+++ frontend/src/components/DisplayQuiz.js
@@ -12,11 +12,13 @@ const DisplayQuiz = () => {
   const base_url="http://localhost:5000/"
   const location = useLocation();
   const navigate=useNavigate()
+  const [attempted,setAttempted]=useState(0)
   const userdata = location.state ? location.state.userdata : null;
   const quizdata = location.state ? location.state.quizdata : null;
   const quiz_id = quizdata ? quizdata._id : null;
   const quizName =quizdata ? quizdata.subjectName:null;
   const description = quizdata ? quizdata.description:null
+  const maxAttempts=quizdata ? quizdata.maxAttempts:null
   const setBy=quizdata ? quizdata.setBy:null
   const usermail = userdata?userdata.email:null
   const username = userdata?userdata.name:null
@@ -69,6 +71,16 @@ const DisplayQuiz = () => {
       return null
     }
   }
+  useEffect(() => {
+    const AttemptedSoFar= async () => {
+      results.map((attempt, index)=>{
+        const cc=attempt.attempt
+        setAttempted(cc>attempted?cc:attempted)
+      })
+    }
+    AttemptedSoFar()
+  })
+   
 
   useEffect(() => {
     const fetchResults = async () => {
@@ -100,6 +112,7 @@ const DisplayQuiz = () => {
 
     fetchResults();
   }, [quizName]);
+
   const renderAttemptHistoryTable = () => {
     return (
       <Table striped bordered hover>
@@ -143,16 +156,26 @@ const DisplayQuiz = () => {
               {/* Render quiz details */}
               {quizName && (
                 <>
+                <div style={{ alignItems: 'center',justifyContent:'left' }}>
                   <p><strong>Quiz name:</strong> {quizName}</p>
                   <p><strong>Description:</strong> {description}</p>
-                    {/* <Button variant="outline-warning" className="w-50" size="sm" onClick={editQuiz} value={JSON.stringify(q)}>Edit</Button>
-                    <Button variant="outline-danger" className="w-50" size="sm" onClick={editQuiz} value={JSON.stringify(q)}>Delete</Button> */}
+                  <p><strong>Max Attempts:</strong> {maxAttempts}</p>
+                  <p><strong>Attempts left:</strong> {maxAttempts-attempted}</p>
+                  </div>
                 <div style={{ display: 'flex', flexDirection: 'column' , alignItems: 'center' }}>
                     {userdata.occupation === "student" ? (
                       <>
-                        <Button variant="success" size = 'sm' className='w-25' onClick={attemptHere}>
-                          Attempt here
-                        </Button>
+                              <Button
+                                variant="success"
+                                size="sm"
+                                className={`w-25 ${maxAttempts - attempted <= 0?"disabled":""}`}
+                                title={maxAttempts - attempted <= 0 ? 'Max attempts reached' : ''}
+                                onClick={attemptHere}
+                                disabled={maxAttempts - attempted <= 0}
+                                
+                              >
+                                Attempt here
+                              </Button>
                         <Button variant="primary" size = 'sm' className="w-25 mt-2 mb-2" onClick={(e) => setShowAttemptHistory(!showAttemptHistory)}>
                           View Attempt History
                         </Button>
